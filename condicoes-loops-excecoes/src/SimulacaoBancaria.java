bbde9aae4e63f82fe0bb7094ca72182f1e862a49
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-05-06 23:33:39 -0300

    adicionando codigos dos desafios na pratica

---
+++ condicoes-loops-excecoes/src/SimulacaoBancaria.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+
+        while (true) {
+            int opcao = scanner.nextInt();
+            switch(opcao){
+                case 1:
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    double valorSaque = scanner.nextDouble();
+                    if(saldo < valorSaque){
+                        System.out.println("Saldo insuficiente.");
+                    } else {
+                        saldo -= valorSaque;
+                        System.out.println("Saldo atual: " + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+}
