c52544016ce49b1d9ce978e3d09271485570c7eb
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-05-07 01:21:13 -0300

    adicionando codigo desafio 4

--- condicoes-loops-excecoes/verificador-cheque-especial/VerificacaoChequeEspecial.java
+++ condicoes-loops-excecoes/verificador-cheque-especial/VerificacaoChequeEspecial.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class VerificacaoChequeEspecial {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+public class VerificacaoChequeEspecial {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         double saldo = scanner.nextDouble();
         double saque = scanner.nextDouble();
         double limiteChequeEspecial = 500;
 
-        // TODO: Verifique se o saque não ultrapassa o saldo disponível na conta:
-
-
-        // TODO: Verifique se o saque ultrapassa o limite do cheque especial, mas não o saldo total disponível:
-
-
-        // Se o saque ultrapassar tanto o saldo disponível quanto o limite do cheque especial
-        System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+        if (saque <= saldo){
+            System.out.println("Transação realizada com sucesso.");
+        } else if (saque < limiteChequeEspecial + saldo) {
+            System.out.println("Transação realizada com sucesso utilizando o cheque especial.");
+        } else{
+            System.out.println("Transação nao realizada. Limite do cheque especial excedido.");
+        }
+        scanner.close();
     }
 }
