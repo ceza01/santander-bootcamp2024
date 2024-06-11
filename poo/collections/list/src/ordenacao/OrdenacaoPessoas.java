75c5618713b8acab5ae213a5d7a61944150b3baa
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-06-11 09:33:39 -0300

    resolucao exercicio ordenacao pessoas

---
+++ poo/collections/list/src/ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,29 @@
+package ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    List<Pessoa> listaPessoas;
+
+    public OrdenacaoPessoas() {
+        this.listaPessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        listaPessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listaPessoas);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(listaPessoas);
+        Collections.sort(listaPessoas, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+}
