887b62fe93a3e5f9d71549851bf9d50745535a88
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-06-14 15:13:26 -0300

    resolucao exercicio agenda eventos map

---
+++ poo/collections/map/src/ordenacao/AgendaEventos.java
@@ -0,0 +1,41 @@
+package ordenacao;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+        if (proximoEvento != null) {
+            System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
+        } else {
+            System.out.println("Não há eventos futuros na agenda.");
+        }
+    }
+}
