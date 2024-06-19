43990ce202727cfe2852c09aeea2c5e21648952f
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-06-19 14:21:17 -0300

    iniciando desafios stream api

--- poo/stream-api/src/stream_api/DesafioUm.java
+++ poo/stream-api/src/stream_api/DesafioUm.java
@@ -1,7 +1,16 @@
 package stream_api;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DesafioUm {
+    /*Desafio 1 - Mostre a lista na ordem numérica:
+    Crie um programa que utilize a Stream API para ordenar a lista de números
+    em ordem crescente e a exiba no console.
+     */
+
     public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
     }
 }
