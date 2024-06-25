d5347d3e7a8f2795e1b31f566956a354e6bae2c3
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2024-06-24 22:07:55 -0300

    adicionando classes dos desafios restantes

---
+++ poo/stream-api/src/Desafio14.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio14 {
+    /*Desafio 14 - Encontre o maior número primo da lista:
+      Com a Stream API, encontre o maior número primo da lista
+      e exiba o resultado no console.
+    */
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+
+    }
+}
---
+++ poo/stream-api/src/Desafio18.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio18 {
+    /*Desafio 18 - Verifique se todos os números da lista são iguais:
+      Utilizando a Stream API, verifique se todos os números da lista são iguais
+      e exiba o resultado no console.
+    */
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+
+    }
+}
